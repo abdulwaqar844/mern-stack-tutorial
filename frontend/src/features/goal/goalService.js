ce3cee9d7cffa13b4c4c2547e279cf7996c0dc5d
Abdul Waqar <63345475+abdulwaqar844@users.noreply.github.com>
2022-05-29 12:25:15 +0500

    Update goalService.js

--- frontend/src/features/goal/goalService.js
+++ frontend/src/features/goal/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${config.apiUrl}/goal/`;
+const API_URL = `http://localhost:4000/goal/`;
 const createGoal = async (goalData, token) => {
   const configurations = {
     headers: {
